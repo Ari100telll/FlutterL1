ff7694917646caed18c5cc104de9131ef1d0b1dc
Nikita Horb <nikita.horb@inoxoft.com>
2021-09-27 21:10:18 +0300

    Add button to switch like icon color.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -31,6 +33,7 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  bool _rowColor = false;
 
   void _pushSaved() {
     Navigator.of(context).push(
@@ -64,7 +67,7 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
+        itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
 
           final index = i ~/ 2;
@@ -76,19 +79,20 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
+    final saved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
       trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.orange : null,
+        saved ? Icons.favorite : null,
+        color: saved ? (_rowColor ? Colors.pink : Colors.orange) : null,
       ),
       onTap: () {
         setState(() {
-          if (alreadySaved) {
+          if (saved) {
             _saved.remove(pair);
           } else {
             _saved.add(pair);
@@ -105,6 +109,16 @@ class _RandomWordsState extends State<RandomWords> {
         title: const Text('Startup Name Generator'),
         actions: [
           IconButton(onPressed: _pushSaved, icon: Icon(Icons.list)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  _rowColor = !_rowColor; // This change Container color
+                });
+              },
+              icon: Icon(
+                Icons.format_color_fill,
+                color: Colors.black,
+              )),
         ],
       ),
       body: _buildSuggestions(),
